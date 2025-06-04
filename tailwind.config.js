57cfc6bba0070d305f9969a5367e4e99c27936ee
usuny0317 <usuny0317@naver.com>
2025-06-04 23:16:30 +0900

    테일윈드 설치치

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // React 파일 경로 포함
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
