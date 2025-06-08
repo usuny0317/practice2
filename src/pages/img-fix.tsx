d9b204ce030c03bc6414a29adac93489c3765918
usuny0317 <usuny0317@naver.com>
2025-06-08 22:46:12 +0900

    ✨ 고정 배경 추가하기

--- src/pages/img-fix.tsx
+++ src/pages/img-fix.tsx
@@ -1,11 +1,13 @@
 const ImgFix = () => {
   return (
-    <div className="relative flex flex-col text-bold text-[#83f97d]">
-      ImgFix
-      <img src="/pika.jpg" />
-      <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white text-black px-6 py-3 rounded shadow-lg">
-        고정 버튼
-      </button>
+    <div className="relative flex items-center justify-center min-h-screen bg-[url('/getbono.jpg')] bg-cover bg-center ">
+      <div className="relative flex flex-col text-bold text-[#83f97d]">
+        ImgFix
+        <img src="/pika.jpg" />
+        <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white text-black px-6 py-3 rounded shadow-lg">
+          고정 버튼
+        </button>
+      </div>
     </div>
   );
 };
