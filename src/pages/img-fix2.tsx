4f2dddc07fb8d3517f0469baf78a533f1694e5b8
usuny0317 <usuny0317@naver.com>
2025-06-10 00:49:01 +0900

    반응형에 맞게게

--- src/pages/img-fix2.tsx
+++ src/pages/img-fix2.tsx
@@ -3,7 +3,7 @@ const ImgFix2 = () => {
     <div className="relative flex flex-col text-bold text-[#83f97d]">
       ImgFix
       <img src="/pika.jpg" />
-      <button className="absolute top-[120px] left-[80px] transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white text-black px-6 py-3 rounded shadow-lg">
+      <button className="absolute top-[50%] left-[80px] transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white text-black px-6 py-3 rounded shadow-lg">
         고정 버튼
       </button>
     </div>
